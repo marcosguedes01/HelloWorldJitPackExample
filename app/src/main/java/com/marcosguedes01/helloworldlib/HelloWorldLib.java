d5541d183a17f415e714c1534919795da9be361d
Marcos Guedes <marcos.guedesjr@hotmail.com>
2019-04-02 17:17:31 -0300

    Arquivos Iniciais

---
+++ app/src/main/java/com/marcosguedes01/helloworldlib/HelloWorldLib.java
@@ -0,0 +1,7 @@
+package com.marcosguedes01.helloworldlib;
+
+public class HelloWorldLib {
+    public static String print(){
+        return "Hello World!";
+    }
+}
---
+++ app/src/test/java/com/marcosguedes01/helloworldlib/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.marcosguedes01.helloworldlib
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun deveriaImprimirHelloWorld() {
+        assertEquals(HelloWorldLib.print(), "Hello World!")
+    }
+}
